bf8306755b0c5924bc0f3d5cadf7334ceda026bc
hientuong4321 <hientuong4321@gmail.com>
2020-05-23 08:41:12 +0700

    JS

---
+++ html/tuhoc/JavaScrip/main.js
@@ -0,0 +1,20 @@
+var fullName = 'Duong Hien Tuong';
+
+
+/**
+ * Một số hàm built-in
+ *  1.Alert
+ *  2.Console (phương thức)
+ *  3.Confirm (Xác nhận)
+ *  4.Prompt (Nhập-tương tự hàm confirm)
+ *  5.Set timout (Sau khoang time se hien ra)
+ *  6.Set interval (Cach khoang time se hien ra dinh ki 1 lan)
+ */
+
+//console.log(fullName);
+
+//prompt('Xac nhan ban du tuoi');
+
+setTimeout(function() {
+    alert('Thong bao');
+}, 1000)
