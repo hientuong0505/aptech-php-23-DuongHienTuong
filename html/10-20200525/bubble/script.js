9c739adb0f3c582758ae851df0300295c5eb3eff
hientuong4321 <hientuong4321@gmail.com>
2020-05-26 10:13:57 +0700

    Bubble

---
+++ html/10-20200525/bubble/script.js
@@ -0,0 +1,7 @@
+function turnOn() {
+    document.getElementById('anh').src='./bat.gif';
+}
+
+function turnOff() {
+    document.getElementById('anh').src='./tat.gif';
+}
---
+++ html/10-20200525/bubble2/js.js
@@ -0,0 +1,11 @@
+var temp=1;
+function changeImg() {
+    
+    if (temp == 1){
+        document.getElementById('anh').src='./bat.gif';
+        temp =2;
+    } else {
+        document.getElementById('anh').src='./tat.gif';
+        temp = 1;
+    }
+}
