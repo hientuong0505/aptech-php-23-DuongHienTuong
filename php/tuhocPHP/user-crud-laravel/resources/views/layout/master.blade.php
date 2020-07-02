72c8f7697c2f1bf0ee1953a41c5b0185c0c8d133
hientuong4321 <hientuong4321@gmail.com>
2020-07-02 11:36:47 +0700

    crud

---
+++ php/tuhocPHP/user-crud-laravel/resources/views/layout/master.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="{{asset('./favicon.ico')}}">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>User CRUD Laravel Demo</title>
+    <link rel="stylesheet" href="{{asset('https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css')}}">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('css/styles.css')}}">
+</head>
+
+<body>
+    @include('layout.header')
+    <hr>
+    @section('content') @show
+    <hr>
+    @include('layout.footer')
+</body>
+
+</html>
---
+++ php/tuhocPHP/user-crud-laravel/resources/views/users/index.blade.php
@@ -0,0 +1,51 @@
+@extends('layout.master')
+@section('content')
+@component('layout.components.title')
+index page
+@endcomponent
+<div class="container mt-3">
+    <div class="row">
+        <div class="col-12">
+            <table class="table table-hover table-bordered">
+                <thead>
+                    <tr class="text-center">
+                        <th scope="col" class="">#</th>
+                        <th scope="col" class="">Name</th>
+                        <th scope="col" class="">Email</th>
+                        <th scope="col" class="">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($users as $user)
+                    <tr class="text-center">
+                        <th scope="row">{{$user->id}}</th>
+                        <td>{{$user->name}}</td>
+                        <td>{{$user->email}}</td>
+                        <td class="d-flex align-items-center justify-content-around">
+                            <form action="{{route('users.show',$user->id)}}" method="get">
+                                <button class="btn btn-sm btn-primary rounded-0">
+                                    Show
+                                </button>
+                            </form>
+                            <form action="{{route('users.edit',$user->id)}}" method="get">
+                                <button class="btn btn-sm btn-warning rounded-0">
+                                    Edit
+                                </button>
+                            </form>
+                            <form action="{{route('users.destroy',$user->id)}}" method="post">
+                                <input type="hidden" name="_token" value="{{csrf_token()}}">
+                                <input type="hidden" name="_method" value="delete">
+                                <button class="btn btn-sm btn-danger rounded-0">
+                                    Delete
+                                </button>
+                            </form>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            <div class="d-flex justify-content-center">{{$users->links()}}</div>
+        </div>
+    </div>
+</div>
+@endsection
